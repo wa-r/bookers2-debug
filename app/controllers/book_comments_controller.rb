a98cc753843ad4a7c90b942c7eaf669dff216a7f
wa-r <nyuuuuun14@gmail.com>
2020-12-20 03:39:04 +0000

    応用課題6 非同期通信(コメント機能)追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,17 +1,19 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
   
   def destroy
-    BookComment.find_by(book_id: params[:book_id]).destroy
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    comment = BookComment.find_by(book_id: params[:book_id])
+    comment.destroy
+    # redirect_back(fallback_location: root_path)
   end
   
   private
